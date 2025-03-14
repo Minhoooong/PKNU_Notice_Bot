7aec49c521ccfd330be5861256fe275b88f831c6
Minhoooong <150977057+Minhoooong@users.noreply.github.com>
2025-03-14 13:50:16 +0900

    Update script.py

--- script.py
+++ script.py
@@ -21,7 +21,7 @@ from aiogram.client.bot import DefaultBotProperties
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, Message
 from bs4 import BeautifulSoup
 from openai import AsyncOpenAI
 
